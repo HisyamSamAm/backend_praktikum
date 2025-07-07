90c1d439484adce4e303f1fc94963e7a9a757f1d
HisyamSamAm <mhisyamnajwan@gmail.com>
2025-07-07 14:31:02 +0700

    Cors for deploy

--- config/cors.go
+++ config/cors.go
@@ -5,6 +5,7 @@ var allowedOrigins = []string{
 	"http://localhost:5173",
 	"http://HisyamSamAm.github.io",
 	"http://127.0.0.1:8088/",
+	"https://frontend-praktikum.vercel.app/",
 }
 
 func GetAllowedOrigins() []string {
